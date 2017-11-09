348270f3f65af17b1098ff0c22dd02dd89666d6f
remifoyard <rfpro8@gmail.com>
2017-11-09 19:36:27 +0100

    2017 11 17 19 36

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
                 <dd><span class="gras"><a href="calculatrice.php">Javascript, HTML, CSS, RESPONSIVE WEB DESIGN</a></span></dd>
             	<hr class="hr_2">
                 <dt><a href="bootstrap/index.html">Bootstrap</a></dt>
-                <dd><span class="gras"><a href="bootstrap/index.html">Bootstrap, Javascript, HTML, CSS</a></span></dd>
+                <dd><span class="gras"><a href="bootstrap/index.html">Bootstrap, Ajax, Javascript, HTML, CSS</a></span></dd>
             	<!--
               <hr class="hr_2">
                 <dt><a href="test.php">Page de Test</a></dt>
